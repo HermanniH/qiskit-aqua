88b36cae55aa904527aac207f518ebb7b09c25cf
HermanniH <7411551+HermanniH@users.noreply.github.com>
2019-11-20 10:09:18 +0900

    Running vqe on real hardware

--- qiskit/chemistry/simple_vqe_test.py
+++ qiskit/chemistry/simple_vqe_test.py
@@ -15,7 +15,7 @@ from qiskit.aqua import QuantumInstance
 from qiskit.aqua.operators import Z2Symmetries
 from qiskit.aqua.algorithms.adaptive import VQE
 from qiskit.aqua.algorithms import ExactEigensolver
-from qiskit.aqua.components.optimizers import SLSQP, L_BFGS_B
+from qiskit.aqua.components.optimizers import SLSQP, L_BFGS_B, SPSA, POWELL,COBYLA
 from qiskit.chemistry.core import Hamiltonian, TransformationType, QubitMappingType
 from qiskit.chemistry.drivers import PySCFDriver, UnitsType
 from qiskit.chemistry.components.variational_forms import UCCSD
@@ -25,15 +25,31 @@ import matplotlib.pyplot as plt
 # allows you to print the progress of simulations
 import logging
 from qiskit.aqua import set_qiskit_aqua_logging
+from qiskit import IBMQ
+import pickle
+from qiskit.providers.aer import noise
 
+
+provider = IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q-community', group='hackathon', project='tokyo-nov-2019')
+#print(provider.backends())
+
+#optimizer = POWELL(maxiter=None, maxfev=1000, disp=False, xtol=0.01, tol=None)
+optimizer = COBYLA(maxiter=200, disp=False, rhobeg=1.0, tol=0.01)
 # provides all information (can be too much text)
 # set_qiskit_aqua_logging(logging.DEBUG)
 
 # provides less information than DEBUG mode
 set_qiskit_aqua_logging(logging.INFO)
 
-rs = np.linspace(0.05,3,25)
+#rs = np.linspace(0.45,1.6,5)
+rs = [1.6]
 energies = []
+angles = []
+machine = 'ibmqx2' #ibmqx2 #ibmq_vigo #'ibmq_ourense'#'ibmq_16_melbourne'
+
+
+
 for r in rs:
     # set molecule and construct its Hamiltonian
     molecule = "H 0.000000 0.000000 0.000000;H 0.000000 0.000000 "+str(r)
@@ -47,8 +63,8 @@ for r in rs:
     qmolecule = driver.run()
 
     core = Hamiltonian(transformation=TransformationType.FULL,
-                            qubit_mapping=QubitMappingType.PARITY, # JORDAN
-                            two_qubit_reduction=True,
+                            qubit_mapping=QubitMappingType.JORDAN_WIGNER, # JORDAN
+                            two_qubit_reduction=False,
                             freeze_core=True,
                             orbital_reduction=[])
     qubit_op, _ = core.run(qmolecule)
@@ -75,7 +91,7 @@ for r in rs:
     the_tapered_op= qubit_op
     # optimizers
     # optimizer = SLSQP(maxiter=1000)
-    optimizer = L_BFGS_B(maxiter=1000)
+    #optimizer = L_BFGS_B(maxiter=1000)
 
     # initial state
     # init_state = HartreeFock(num_qubits=the_tapered_op.num_qubits,
@@ -115,19 +131,39 @@ for r in rs:
     # set up VQE
     algo = VQE(the_tapered_op, var_form, optimizer)
 
+
+    # For simulating classically with noise
     # Choose the backend (use Aer instead of BasicAer)
-    #backend = Aer.get_backend('statevector_simulator')
-    backend = Aer.get_backend('qasm_simulator')
-    quantum_instance = QuantumInstance(backend=backend)
+    # backend = Aer.get_backend('qasm_simulator')
+    # provider = IBMQ.load_account()
+    # provider = IBMQ.get_provider(hub='ibm-q-community', group='hackathon', project='tokyo-nov-2019')
+    # backend_sim = provider.get_backend(machine)
+    # backendConfig = backend_sim.configuration()
+    # properties = backend_sim.properties()
+    # coupling_map = backendConfig.coupling_map
+    # noise_model = noise.device.basic_device_noise_model(properties)
+    # basis_gates = noise_model.basis_gates
+    #quantum_instance = QuantumInstance(backend=backend, optimization_level=0, shots = 3000,noise_model=noise_model,basis_gates=basis_gates)#, initial_layout=[3,4])
+
+
+    #For running on real hardware
+    #backend = IBMQ.get_provider().get_backend('machine')
+    backend = provider.get_backend(machine)
+    quantum_instance = QuantumInstance(backend=backend, optimization_level=3, shots = 3000)
 
     # run the algorithm
     algo_result = algo.run(quantum_instance)
 
     # get the results
     _, result = core.process_algorithm_result(algo_result)
+    print(result)
 
     energy = result['energy']
     energies.append(energy)
+    angles.append(result['algorithm_retvals']['opt_params'])
+outfile = open('real_H2_16.pcl','wb')
+pickle.dump([rs,energies,angles],outfile)
+outfile.close()
 
 plt.plot(rs,energies)
 plt.show()
